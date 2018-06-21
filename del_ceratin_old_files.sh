f85870a0aaac78a3978b08d4402712ba25ac454f
Max Bergmann <contact@maxjbergmann.com>
2018-06-21 21:10:20 +0200

    Create del_ceratin_old_files.sh

---
+++ del_ceratin_old_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#MP4 and JPEG files older than 10 days in a directory get deleted
+
+DIRECTROY"/path/to/dir"
+FILE0=".mp4
+FILE1=".jpg"
+DEL_TIME="9"
+
+find ${DIRECTROY} -type f \( -iname \*${FILE0}\* -o -iname \*${FILE1}\* \) -mtime +${DEL_TIME} -exec rm -f {} +
