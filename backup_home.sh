34053862786d595a48c1e70c16ab2df7d1f22906
Max Bergmann <contact@maxjbergmann.com>
2018-06-25 19:21:07 +0200

    Create backup_home.sh

---
+++ backup_home.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USER="<user_name>"
+DESTINATION="<path/to/destination/>"
+
+
+rsync -avc --no-perms \
+/home/$USER/Documents/ \
+/home/$USER/Downloads/ \
+/home/$USER/Pictures/ \
+/home/$USER/Videos/ \
+/home/$USER/Music/ \
+/home/$USER/KeePass/ \
+/home/$USER/.ssh/ \
+--exclude '*.img' \
+--exclude '*.iso' \
+$DESTINATION
