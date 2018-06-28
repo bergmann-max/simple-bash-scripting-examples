4c08f56768adaac48a7b66a8f33fa6d6b8654819
Max Bergmann <contact@maxjbergmann.com>
2018-06-28 13:51:39 +0200

    Update is_something_running.sh

--- is_something_running.sh
+++ is_something_running.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# This script checks if the example_script.sh script is running. If not it'll start it.
+# This script checks if the example_script.sh script is running.
 
 # Change the directory of this script
 BASEDIR=$(dirname "$0")
 cd "${BASEDIR}" || exit
 
-RESULT=$(ps aux | grep --ignore-case "example_script.sh" | grep --invert-match "grep" | wc --lines)
+PROCESS="example_script.sh"
+
+RESULT=$(pgrep --ignore-case "$PROCESS" | grep --count --invert-match "grep" )
 if [ "$RESULT" -eq 0 ] ; then
 
-  ./example_script.sh
+        echo "${PROCESS} is not  running"
 
+else
+
+        echo "${PROCESS} is running"
+        
 fi
