7663f3d3619da000138e6146ba16ac689f26bd19
Max Bergmann <contact@maxjbergmann.com>
2018-06-28 13:54:52 +0200

    Update rm_ceratin_old_files.sh

--- rm_ceratin_old_files.sh
+++ rm_ceratin_old_files.sh
@@ -3,8 +3,7 @@
 #MP4 and JPEG files older than 10 days in a directory get deleted
 
 DIRECTROY"/path/to/dir"
-FILE0=".mp4
+FILE0=".mp4"
 FILE1=".jpg"
 DEL_TIME="9"
-
-find ${DIRECTROY} -type f \( -iname \*${FILE0}\* -o -iname \*${FILE1}\* \) -mtime +${DEL_TIME} -exec rm -f {} +
+find $"{DIRECTROY}" -type f \( -iname \*${FILE0}\* -o -iname \*${FILE1}\* \) -mtime +${DEL_TIME} -exec rm -f {} +
