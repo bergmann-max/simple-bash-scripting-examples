d67e523ff1467aca4b2b009140fa069ca6acb5f0
Max Bergmann <contact@maxjbergmann.com>
2018-06-28 12:23:00 +0200

    Create show_every_12th_number_from_0-1000.sh

---
+++ show_every_12th_number_from_0-1000.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#show every 12th number from 0-1000
+
+index = 0
+for x in $(seq 1 1000)
+do
+    if ! ((index % 12)); then
+        echo "$index divisible by 12."
+    fi
+    index=$((index + 1))
+done
