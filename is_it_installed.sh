d7e96a789baa8122e33db4c15a85392f77f14672
Max Bergmann <contact@maxjbergmann.com>
2018-07-04 03:36:37 +0200

    Update is_it_installed.sh

--- is_it_installed.sh
+++ is_it_installed.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 # Is something installed?
 
-echo -e "\n Prüfen ob alle benötigten Programme installiert sind:\n"
+echo -e "\n Check if needed packages are installed:\n"
 
 # return 1 if global command line program installed, else 0
 # example
@@ -9,7 +11,7 @@ function program_is_installed {
   # set to 1 initially
   local return_=1
   # set to 0 if not found
-  type $1 >/dev/null 2>&1 || { local return_=0; }
+  type "$1" >/dev/null 2>&1 || { local return_=0; }
   # return value
   echo "$return_"
 }
@@ -19,7 +21,7 @@ function program_is_installed {
 # echo echo_fail "No"
 function echo_fail {
   # echo first argument in red
-  printf "\e[31m✘ ${1} Dieses Programm ist nicht installiert wird aber benötigt"
+  printf "\e[31m✘ ${1} This package isn't installed"
   # reset colours back to normal
   echo -e "\033[0m"
 }
@@ -39,14 +41,14 @@ function echo_pass {
 # echo echo_if 1 "Passed"
 # echo echo_if 0 "Failed"
 function echo_if {
-  if [ $1 == 1 ]; then
-    echo_pass $2
+  if [ "$1" == 1 ]; then
+    echo_pass "$2"
   else
-    echo_fail $2
+    echo_fail "$2"
   fi
 }
 
 # command line programs
-echo "wget    $(echo_if $(program_is_installed wget))"
-echo "pwd    $(echo_if $(program_is_installed pwd))"
-echo "xTest $(echo_if $(program_is_installed xTest))"
+echo "wget    $(echo_if "$(program_is_installed wget)")"
+echo "pwd    $(echo_if "$(program_is_installed pwd)")"
+echo "xTest $(echo_if "$(program_is_installed xTest)")"
