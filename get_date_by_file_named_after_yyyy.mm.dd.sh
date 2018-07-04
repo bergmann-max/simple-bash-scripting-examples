7d0ff1143c1e7cbdcb6cc66dc2b58aa08a9edf6f
Max Bergmann <contact@maxjbergmann.com>
2018-07-04 03:50:17 +0200

    Update get_date_by_file_named_after_yyyy.mm.dd.sh

--- get_date_by_file_named_after_yyyy.mm.dd.sh
+++ get_date_by_file_named_after_yyyy.mm.dd.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-#get date by a mp4 file named after [yyy][mm] %Y%m.mp4
+#get date by a mp4 file named after [yyyy][mm] %Y%m.mp4
 
 
 DIRECTORY="/path/to/dir"
-MOVIES=$( ls ${DIRECTORY} | grep .mp4 | wc -l )
+MOVIES=$( find ${DIRECTORY} | grepc .mp4 )
 MOVIENUMBER=${MOVIES}
 COUNTER=0
 
 
-while [  ${COUNTER} -lt ${MOVIES} ]; do
+while [  "${COUNTER}" -lt "${MOVIES}" ]; do
 
-		VIDEONAME=$(ls -t ${inputdir0} | tail -n ${MOVIENUMBER} | head -n 1)	#List all files invert
+		VIDEONAME=$(ls -t "${DIRECTORY}" | tail -n "${MOVIENUMBER}" | head -n 1)	#List all files invert
 		VIDEOSTRING="${VIDEONAME%.*}"	#cut file name at "."
 
-		VIDEODATE=$(basename ${VIDEOSTRING} ) #cut the path to the file
+		VIDEODATE=$(basename "${VIDEOSTRING}" ) #cut the path to the file
 
-		MYDATE=$(date -d ${VIDEODATE}01 +'%Y%m%d') #Put "01" to the string to detect date in the next step
+		MYDATE=$(date -d "${VIDEODATE}"01 +'%Y%m%d') #Put "01" to the string to detect date in the next step
 
-		MONTHNAME=$(date -d ${MYDATE} +%B) #Get the date from a string
+		MONTHNAME=$(date -d "${MYDATE}" +%B) #Get the date from a string
 
-		echo ${MONTHNAME}
+		echo "${MONTHNAME}"
 
 	MOVIENUMBER=$((MOVIENUMBER - 1))
 	COUNTER=$((COUNTER + 1))
