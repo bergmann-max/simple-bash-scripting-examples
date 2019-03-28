18b5235402884f6e38014eae2065926b857fc5a1
Max Bergmann <contact@maxjbergmann.com>
2019-03-28 22:03:53 +0100

    Update select_menu.sh

--- select_menu.sh
+++ select_menu.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 
-PS3='Please enter your choice: '
-OPTIONS=("Option 1" "Option 2" "Option 3" "Quit")
+PS3='What do you whant to eat?: '
+OPTIONS=("Pizza" "Hamburger" "Ice Cream" "Quit")
 select OPT in "${OPTIONS[@]}"
 do
     case $OPT in
-        "Option 1")
-            CHOICE='1' && break
+        "Pizza")
+            CHOICE='pizza' && break
             ;;
-        "Option 2")
-            CHOICE='2' && break
+        "Hamburger")
+            CHOICE='hamburger' && break
             ;;
-        "Option 3")
-            CHOICE='3' && break
+        "Ice Cream")
+            CHOICE='ice cream' && break
             ;;
         "Quit")
             break
             ;;
-        *) printf "\ninvalid option ${REPLY}\n";;
+        *) printf "\nSorry, we dont serve ${REPLY}\n";;
     esac
 done
 
 
-printf '\n'${CHOICE}' selected\n'
+printf "\nYou get a ${CHOICE}\n"
